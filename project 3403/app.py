b6e17a7e0ff56145a092c8b79ef4e894dbf2740b
crocodilebaby <997940508crj@gmail.com>
2023-05-17 15:41:53 +0800

    change

--- project 3403/app.py
+++ project 3403/app.py
@@ -1,9 +1,37 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
-    return render_template("register.html")
+    return render_template("login.html")
 
+@app.route("/re", methods=("GET", "POST"))
+def register():
+    return render_template("register.html")
 app.run()
+
+@app.route('/',methods=['GET','POST'])
+def register():
+    # request:请求对象 -->获取请求方式、数据
+    if request.method == 'POST':
+        # 获取请求参数
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        if not all([username,password,password2]):
+            # print('数据不完整')
+            flash('数据不完整')
+
+        elif password != password2:
+            # print('密码不一致')
+            flash('密码不一致')
+        else:
+            # print('完成')
+            return 'success'
+    return render_template('index.html')
+    # return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
--- project 3403/bot.py
+++ project 3403/bot.py
@@ -1,4 +1,4 @@
-import flask from Flask, redirect, render_template, request, url_for
+import flask from Flask,redirect, render_template, request, url_for
 import openai from openai
 
 app =(__name__)
@@ -6,3 +6,4 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/chatgpt", methods=("GET", "POST"))
 def generate:
+
